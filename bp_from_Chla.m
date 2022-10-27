b8766fca37498ffc8bf07b5ed4e685648f16941a
Matthew Kehrli <mdkehrli@ucsd.edu>
2022-10-27 15:45:24 -0700

    Update bp_from_Chla.m

--- bp_from_Chla.m
+++ bp_from_Chla.m
@@ -1,5 +1,5 @@
 function [bp] = bp_from_Chla(Chla,lambda)
-% Implement the  empirical relationship from Morel and Maritorena 2001 to
+% Implement the empirical relationship from Morel and Maritorena 2001 to
 % calculate the particulate scattering coefficient (bp) at preselected
 % output light wavelengths (lambda) using the input value of chlorophyll-a
 % concentration (Chla)
